2a50ef05c74c2ccaadb1596ea3dc65dcbf5964f8
Adsryen <prl1594959462@gmail.com>
2026-03-08 06:18:53 +0800

    chore: 注册冰箱模块到应用
    - 在应用初始化中注册冰箱路由
    - 导出 Fridge 模型
    - 导出 fridge_service 服务

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,6 @@
 from app.models.user import User
 from app.models.item import Item
 from app.models.system_settings import SystemSettings
+from app.models.fridge import Fridge
 
-__all__ = ['User', 'Item', 'SystemSettings']
+__all__ = ['User', 'Item', 'SystemSettings', 'Fridge']
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
 """业务逻辑服务"""
+from app.services.fridge_service import FridgeService
+
+__all__ = ['FridgeService']
